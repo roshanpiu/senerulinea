d4bd6ffabde6baf67125a82908d2e6eece9332e2
Roshan <Roshan>
2016-06-24 11:40:53 +0530

    interface change

---
+++ routes/brandix_routes.js
@@ -0,0 +1,41 @@
+module.exports = function(app) {
+
+    app.post('/brandixData', function (req, res) {
+        // var newUser = new User(req.body);
+        // newUser.save(function(err, obj) {
+        //     if (err) {
+        //         return res.json({info: 'error during user create', error: err});
+        //     };
+        //     return res.json({info: 'user created successfully', obj: obj});
+        // });
+        console.log(JSON.stringify(req.body));
+        res.json({requestBody : req.body});
+
+    });
+
+    
+    app.get('/brandixData', function (req, res) {
+        // User.find(function(err, users) {
+        //     if (err) {
+        //         return res.json({info: 'error during find users', error: err});
+        //     };
+        //     return res.json({info: 'users found successfully', data: users});
+        // });
+        res.json({msg: "get all"});
+    });
+    
+    app.get('/brandixData/:id', function (req, res) {
+        // User.findById(req.params.id, function(err, user) {
+        //     if (err) {
+        //         return res.json({info: 'error during find user', error: err});
+        //     };
+        //     if (user) {
+        //         return res.json({info: 'user found successfully', data: user});
+        //     } else {
+        //         return res.json({info: 'user not found'});
+        //     }
+        // });
+        res.json({msg: "get one"});
+    });
+
+};
--- server.js
+++ server.js
@@ -2,8 +2,18 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+var bodyParser = require('body-parser');
+var path = require('path');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/*', express.static(path.join(__dirname, 'public/index.html')));
+
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://roshan:123@ds035623.mlab.com:35623/brandix');
+var db = mongoose.connection;
 
 
 app.get('/ping', function(req, res) {
@@ -13,10 +23,15 @@ app.get('/ping', function(req, res) {
     });
 });
 
+var PORT = 3007;
 
+var brandix = require('./routes/brandix_routes.js')(app);
 
-var port = process.env.PORT || 3007;
-
-app.listen(port, function() {
-    console.log("The server is running on port " + port);
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    var server = app.listen(process.env.PORT || PORT,function(){
+        console.log("Connected to the DB Server is running at port " + PORT);
+    });
 });
+
+
